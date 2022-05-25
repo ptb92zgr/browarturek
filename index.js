3b07b5bf121b54b6c3291e22f7c53f3b9fd32f8a
Patryk Janicki <patrykjanicki@icloud.com>
2022-05-25 15:08:33 +0200

    need to make sliders and add media quaries

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const toggleMenuBtn = document.querySelector(".menu__btn")
 const menuItems = document.querySelector(".menu__items")
 const menuLink = document.querySelectorAll("menu__link")
-const paralaxItem = document.querySelector(".welcome")
+const sections = document.querySelectorAll(".section")
 
 const showMenu = () => {
 	menuItems.classList.toggle("show__menu")
@@ -10,6 +10,16 @@ const showMenu = () => {
 const hideMenu = () => {
 	menuItems.classList.toggle("show__menu")
 }
+
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		entry.target.classList.toggle("appear", entry.isIntersecting)
+	})
+})
+
+sections.forEach(section => {
+	observer.observe(section)
+})
 
 toggleMenuBtn.addEventListener("click", showMenu)
 menuItems.addEventListener("click", showMenu)
